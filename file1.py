d0d09e0933fcfe206d79e72fb3d434f6fd621379
AlfonsoFdzGrby <fernandezgaribayalfonsomaron@gmail.com>
2024-02-17 18:44:41 -0600

    17-02-24 / 7:00 PM

---
+++ file1.py
@@ -0,0 +1,5 @@
+print("Este es el repositorio de Poncho :D")
+var = 59
+print("El valor de la variable es: " + str(var))
+var = 60
+print("El valor de la variable ha cambiado y ahora es: " + str(var))
